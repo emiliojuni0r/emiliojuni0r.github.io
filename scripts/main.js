af7356217c28ab0017e92b8023748009d6cf8830
emiliojuni0r <146708041+emiliojuni0r@users.noreply.github.com>
2023-11-12 22:19:03 +0700

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
+// toggle n responsive nav
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navList = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navList.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
+
 // clear form before unload
 window.onbeforeunload = () => {
     for (const form of document.getElementsByTagName("form")) {
